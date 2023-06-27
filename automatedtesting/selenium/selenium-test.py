b24e9f3794684c4a575867a7cfbed992cd9d5445
Andrew Murray <andrew.murray@arvato.com>
2023-06-28 00:14:10 +0300

    selenium

--- automatedtesting/selenium/selenium-test.py
+++ automatedtesting/selenium/selenium-test.py
@@ -1,6 +1,7 @@
 # #!/usr/bin/env python
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 import datetime
 
